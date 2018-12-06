e2616c9697ed0e99a5419628843fce966dff55e6
Piotr Woszczek <p.woszczek@volvo.com>
2018-12-06 20:56:45 +0100

    Fix Foreground Service being killed if startForeground is not called after 5 secs !!!

--- app/src/main/java/pl/coddev/applu/i/DataService.java
+++ app/src/main/java/pl/coddev/applu/i/DataService.java
@@ -1,6 +1,7 @@
 package pl.coddev.applu.i;
 
 import android.annotation.TargetApi;
+import android.app.Notification;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -35,6 +36,11 @@ public class DataService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG, "onStartCommand()");
+        int NOTIFICATION_ID = (int) (System.currentTimeMillis()%10000);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForeground(NOTIFICATION_ID, new Notification.Builder(this, "channel1")
+                    .build());
+        }
         return Service.START_STICKY;
     }
 
@@ -44,31 +50,33 @@ public class DataService extends Service {
         Log.d(TAG, "onCreate()");
         super.onCreate();
         context = getApplicationContext();
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                PackageManager pm = getApplicationContext().getPackageManager();
-                // one of the most consuming tasks
-                ArrayList<PackageInfo> packs = (ArrayList) pm.getInstalledPackages(0);
-                //PInfoHandler.setAllPInfos();
-                ArrayList<PInfo> allInfos = new ArrayList<>();
-                for (int i = 0; i < packs.size(); i++) {
-                    PackageInfo pi = packs.get(i);
+        AsyncTask.execute(() -> {
+            PackageManager pm = getApplicationContext().getPackageManager();
+            // one of the most consuming tasks
+            ArrayList<PackageInfo> packs = (ArrayList) pm.getInstalledPackages(0);
+            //PInfoHandler.setAllPInfos();
+            ArrayList<PInfo> allInfos = new ArrayList<>();
+            for (int i = 0; i < packs.size(); i++) {
+                PackageInfo pi = packs.get(i);
 
-                    if (!PInfoHandler.fallsIntoSelector(pi, UninstallWidget.AppSelectorStatus.ALL, context))
-                        continue;
+                if (!PInfoHandler.fallsIntoSelector(pi, UninstallWidget.AppSelectorStatus.ALL, context))
+                    continue;
 
-                    PInfo newInfo = new PInfo();
-                    // most time consuming task!
-                    newInfo.setAppname(pi.applicationInfo.loadLabel(pm).toString());
-                    newInfo.setIsSystemPackage(PInfoHandler.isSystemPackage(pi));
-                    newInfo.setPname(pi.packageName);
-                    if(!allInfos.contains(newInfo))allInfos.add(newInfo);
-                }
-                // add all to synchronized ArrayList, not one by one
-                PInfoHandler.setAllPInfos(allInfos);
-                Log.d(TAG, "onCreate " + packs.size());
+                PInfo newInfo = new PInfo();
+                // most time consuming task!
+                newInfo.setAppname(pi.applicationInfo.loadLabel(pm).toString());
+                newInfo.setIsSystemPackage(PInfoHandler.isSystemPackage(pi));
+                newInfo.setPname(pi.packageName);
+                if(!allInfos.contains(newInfo))allInfos.add(newInfo);
             }
+            // add all to synchronized ArrayList, not one by one
+            PInfoHandler.setAllPInfos(allInfos);
+            Log.d(TAG, "onCreate " + packs.size());
         });
+        int NOTIFICATION_ID = (int) (System.currentTimeMillis()%10000);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForeground(NOTIFICATION_ID, new Notification.Builder(this, "channel1")
+                    .build());
+        }
     }
 }
