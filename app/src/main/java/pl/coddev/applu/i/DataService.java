5c9a79377754a7c0264170667fedb67b951146c7
Piotr Woszczek <p.woszczek@volvo.com>
2019-01-03 21:18:29 +0100

    Fix start notification issues on Android 8.1+

--- app/src/main/java/pl/coddev/applu/i/DataService.java
+++ app/src/main/java/pl/coddev/applu/i/DataService.java
@@ -2,23 +2,31 @@ package pl.coddev.applu.i;
 
 import android.annotation.TargetApi;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
 
+import pl.coddev.applu.R;
 import pl.coddev.applu.b.PInfoHandler;
 import pl.coddev.applu.d.PInfo;
 import pl.coddev.applu.p.UninstallWidget;
+
+import static android.support.v4.app.NotificationCompat.PRIORITY_MIN;
 
 
 public class DataService extends Service {
@@ -36,11 +44,9 @@ public class DataService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG, "onStartCommand()");
-        int NOTIFICATION_ID = (int) (System.currentTimeMillis()%10000);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForeground(NOTIFICATION_ID, new Notification.Builder(this, "channel1")
-                    .build());
-        }
+
+        startForeground();
+
         return Service.START_STICKY;
     }
 
@@ -73,10 +79,34 @@ public class DataService extends Service {
             PInfoHandler.setAllPInfos(allInfos);
             Log.d(TAG, "onCreate " + packs.size());
         });
-        int NOTIFICATION_ID = (int) (System.currentTimeMillis()%10000);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForeground(NOTIFICATION_ID, new Notification.Builder(this, "channel1")
-                    .build());
-        }
+        startForeground();
+    }
+
+    private void startForeground() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            startMyOwnForeground();
+        else
+            startForeground(1, new Notification());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void startMyOwnForeground(){
+        String NOTIFICATION_CHANNEL_ID = "com.example.simpleapp";
+        String channelName = "My Background Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
     }
 }
